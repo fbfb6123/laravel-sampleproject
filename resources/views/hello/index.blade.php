26bca2e72dcd1829d6e05bbf72d38a75180244dc
池田 光太朗 <koutaro_ikeda@s-cubed.co.jp>
2020-09-17 10:00:54 +0900

    Request/Response

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -5,6 +5,19 @@
 </head>
 <body>
 <h1>Hello/Index</h1>
-<p>{{$msg}}</p>
+<p>{{!!$msg!!}}</p>
+<form action="/hello" method="post">
+    @csrf
+    <div>NAME:<input type="text" name="name"></div>
+    <div>MAIL:<input type="text" name="mail"></div>
+    <div>TEL:<input type="text" name="tel"></div>
+    <input type="submit">
+</form>
+<hr>
+<ol>
+    @for($i = 0;$i < count($keys);$i++)
+        <li>{{$keys[$i]}}:{{$values[$i]}}</li>
+    @endfor
+</ol>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Middleware\HelloMiddleware;
 
 
 
-Route::middleware([HelloMiddleware::class])->group(function (){
+//Route::middleware([HelloMiddleware::class])->group(function (){
 Route::get('/hello','HelloController@index');
-Route::get('/hello/other', 'HelloController@other');
-});
+Route::post('/hello', 'HelloController@index');
+//});
