554be1b33f36c6dc8d288cb0ea70cccca11174ac
fbfb6123 <fbfb6123@gmail.com>
2020-10-02 16:54:41 +0900

    migrations/2020_10_02_075119_create_temp_tables_table.php

--- app/TempTable.php
+++ app/TempTable.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class UserTest extends Model
+class TempTable extends Model
 {
-
+    //
 }
--- database/migrations/2020_10_02_075119_create_temp_tables_table.php
+++ database/migrations/2020_10_02_075119_create_temp_tables_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserTestTable extends Migration
+class CreateTempTablesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateUserTestTable extends Migration
      */
     public function up()
     {
-        Schema::create('usertest', function (Blueprint $table) {
+        Schema::create('temp_tables', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('mail');
-            $table->integer('age');
+            $table->string('title', 20);
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ class CreateUserTestTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('usertest');
+        Schema::dropIfExists('temp_tables');
     }
 }
