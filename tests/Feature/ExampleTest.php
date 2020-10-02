74a179e56e41b9030840420e5d635af90e032d18
fbfb6123 <fbfb6123@gmail.com>
2020-10-02 10:21:13 +0900

    assertSeeInOrder

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -20,6 +20,9 @@ class ExampleTest extends TestCase
         $this->get('/hello')->assertOK();
         //hogeという存在しないページにアクセスした際に404でページが検出されるか
         $this->get('/hoge')->assertStatus(404);
-
+        //指定したpathに<h1>が含まれているか
+        $this->get('/hello')->assertSee('<h1>');
+        //指定したpathに用意したテキストが順に従って登場することを確認
+        $this->get('/hello')->assertSeeInOrder(['<html','<head','<body','<h1>']);
     }
 }
