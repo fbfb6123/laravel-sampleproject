62b92c456d78ed54b00e04fe988e0f9accc323d3
fbfb6123 <fbfb6123@gmail.com>
2020-10-02 14:52:19 +0900

    create.action

--- resources/views/hello/create.blade.php
+++ resources/views/hello/create.blade.php
@@ -1,1 +1,16 @@
-test.create
+<!doctype html>
+<html lang="ja">
+<head>
+    <title>Index</title>
+</head>
+<body>
+<h1>Hello/Index</h1>
+<form action="/hello/store" method="post">
+    @csrf
+    <div>NAME:<input type="text" name="name"></div>
+    <div>MAIL:<input type="text" name="mail"></div>
+    <div>age:<input type="text" name="age"></div>
+    <input type="submit">
+</form>
+</body>
+</html>
